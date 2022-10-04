fa64ae3abdcbbca6a8dbe5b41d71012c78675343
iamVisheshSharma <61023453+iamVisheshSharma@users.noreply.github.com>
2022-10-05 00:27:59 +0530

    Update index.js
    Update index.js for swagger.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert { type: "json" };
 import { connectDB } from './config/db.js';
 import upload from './routes/uploadFile.js';
 import download from './routes/download.js';
@@ -11,6 +13,7 @@ dotenv.config();
 
 const app = express();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
 app.use('/ushare', upload);
